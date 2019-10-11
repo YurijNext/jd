cb468fa0f48af55d175256e603281509ce64a733
AlexemDev <Alpatov.dev@gmail.com>
2019-10-11 16:30:12 +0300

    d

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     $('.story__slider').slick({
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',
@@ -24,32 +25,100 @@ $(document).ready(function () {
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 1000,
                 settings: {
                     arrows: true,
                     centerMode: true,
-                    centerPadding: '40px',
+                    centerPadding: '0px',
                     slidesToShow: 3
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 840,
+                settings: {
+                    arrows: true,
+                    centerMode: false,
+                    centerPadding: '0px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 640,
                 settings: {
                 arrows: true,
-                centerMode: true,
-                centerPadding: '40px',
+                centerMode: false,
+                centerPadding: '0px',
                 slidesToShow: 1
                 }
             }
         ]
     });
 
+    function scrollToId(str) {
+        $(str + '[href*="#"]').on('click', function(e) {
+            e.preventDefault()
+    
+            $('html, body').animate(
+                {
+                    scrollTop: $($(this).attr('href')).offset().top,
+                },
+                500,
+                'linear'
+            )
+        })
+    }
+
+    scrollToId('.navigation__link');
+
+    (function mobileMenu() {
+
+        const   openBtn  = $('.mobile-menu__button'),
+                closeBtn = $('.mobile-menu__close'),
+                menu     = $('.mobile-menu'),
+                navList  = $('.mobile-navigation__list');
+
+        
+        openBtn.on('click', function(e) {
+
+            e.preventDefault();
+            menu.fadeIn(300);
+
+        });
+
+        closeBtn.on('click', function(e) {
+
+            e.preventDefault();
+            menu.fadeOut(300);
+
+        });
+
+        $(document).keypress(function(e) {
+
+            if(e.which == 27)
+                menu.fadeOut(300)
+
+        });
+
+        navList.on('click', function(e) {
+
+            let target = e.target;
+
+            if(target.tagName === 'A') {
+                menu.fadeOut(300);
+                setTimeout( scrollToId('.mobile-navigation__link'), 500);
+            }
+        });
+
+
+
+    }());
+
     (function slidesPugination() {
         const   pagcurrentSlide = $('.current-slide'),
-                pagAllSlides = $('.all-slides'),
-                sliderItem = $('.slider__item'),
-                slickArrow = $('.slick-arrow'),
-                lastItemData = sliderItem.last().attr('data-id');
+                pagAllSlides    = $('.all-slides'),
+                sliderItem      = $('.slider__item'),
+                slickArrow      = $('.slick-arrow'),
+                lastItemData    = sliderItem.last().attr('data-id');
 
         pagAllSlides.html(lastItemData);
 
@@ -63,10 +132,60 @@ $(document).ready(function () {
     $('.countdown').downCount({
         date: '09/25/2020 12:00:00',
         offset: +10
-    }, function () {
-        alert('WOOT WOOT, done!');
     });
 
+    (function scrollTop() {
+
+        const btn = $('#toTop');
+
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 300) {
+                btn.fadeIn();
+            } else {
+                btn.fadeOut();
+            }
+        });
+
+        btn.on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({scrollTop:0}, '300');
+        });
+
+    }());
+
+    (function inputsViewController() {
+
+        let placeholder = '';
+
+        $('.dropdown').on('click', function() {
+            $(this).attr('tabindex', 1).focus();
+            $(this).toggleClass('active');
+            $(this).find('.dropdown-menu').slideToggle(300);
+        });
+    
+        $('.dropdown').focusout(function () {
+            $(this).removeClass('active');
+            $(this).find('.dropdown-menu').slideUp(300);
+        });
+    
+        $('.dropdown .dropdown-menu li').click(function () {
+            $(this).parents('.dropdown').find('span').text($(this).text());
+            $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+        });
+    
+        $('input').focusin(function() {
+            placeholder = $(this).attr('placeholder');
+            $(this).attr('placeholder', '');
+            $(this).siblings('label').css('display', 'block');
+        });
+    
+        $('input').focusout(function() {
+            $(this).attr('placeholder', placeholder);
+            $(this).siblings('label').css('display', 'none');
+        });
+    
+
+    }());
 
 // Inite Yandex Maps
     (function initeMap() {
